61a096013294961e02917f33fa792d73dea13813
Kim Dahee <ekekekgml@naver.com>
2020-04-17 11:24:31 +0900

    New Program

---
+++ 하찮은문제들.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	int result = 1;
+
+	for (int i = 0; i < k; i++) 
+	{
+		result *= (n - i);
+	}
+
+	for (int i = 1; i < k + 1; i++)
+	{
+		result /= i;
+	}
+
+	cout << result;
+
+}
